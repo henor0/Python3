ea0d6ac2f895a86d5066bc8052d2d55513c33dd2
Student <henergashi2019@gmail.com>
2024-12-07 03:11:00 -0800

    ...

---
+++ Lesson7/Index.py
@@ -0,0 +1,17 @@
+age = 25
+
+int_as_str = str(age)
+
+print(int_as_str, "of type", type(int_as_str))
+
+
+print(bool("")) # Output: False
+print(bool("50")) # Output: True
+
+print(bool([""])) # Output: True
+print(bool(None)) # Output: False
+
+x = 85
+y = 9.5
+resultat = x+y
+print(resultat, "of type",type(x+y))
