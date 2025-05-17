f9075c4cb1cdbedb66f3ed5d637b12e24ff37ed9
Student <henergashi2019@gmail.com>
2025-05-17 02:39:47 -0700

    ...

---
+++ Ushtrime/index.py
@@ -0,0 +1,17 @@
+data = {
+    "name": "John Marston",
+    "Age": 31,
+"adrees": {
+    "street": "11234",
+    "city": "Prishtine"
+},
+"contact":[
+    {
+        "type": "email"
+    },
+    {
+        "type": "phone"
+    }
+   ]
+}
+print(data["name"])
